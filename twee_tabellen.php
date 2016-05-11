d595ea30108e121c70435e07ea12a796ea182c05
Rwinning <erwin.van.tilburg@hotmail,com>
2016-05-11 14:15:21 +0200

    tabel achtergrond kleur

--- twee_tabellen.php
+++ twee_tabellen.php
@@ -7,25 +7,89 @@
     <body>
         <h3>Twee tabellen</h3>
 
-        <h5>Tabel 1</h5>
         <table id="tbl1">
         <tr>
-            <td>Optie1</td>
-            <td>Optie2</td>
-            <td>Optie3</td>
-            <td>Optie4</td>
-            <td>Optie5</td>
+            <th>Name</th>
+            <th>Name</th>
+            <th>Name</th>
+            <th>Name</th>
+            <th>Name</th>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+        </tr>
+        <tr>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+        </tr>
+        <tr>
+            <td>Halo</td>
+            <td>Halo</td>
+            <td>Halo</td>
+            <td>Halo</td>
+            <td>Halo</td>
+            <td>Halo</td>
+        </tr>
+        <tr>
+            <td>Reach</td>
+            <td>Reach</td>
+            <td>Reach</td>
+            <td>Reach</td>
+            <td>Reach</td>
+            <td>Reach</td>
         </tr>
         </table>
 
-        <h5>Tabel 2</h5>
         <table id="tbl2">
         <tr>
-            <td>Optie1</td>
-            <td>Optie2</td>
-            <td>Optie3</td>
-            <td>Optie4</td>
-            <td>Optie5</td>
+            <th>Name</th>
+            <th>Name</th>
+            <th>Name</th>
+            <th>Name</th>
+            <th>Name</th>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+            <td>Shotgun</td>
+        </tr>
+        <tr>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+            <td>Blood Gultch</td>
+        </tr>
+        <tr>
+            <td>Halo</td>
+            <td>Halo</td>
+            <td>Halo</td>
+            <td>Halo</td>
+            <td>Halo</td>
+            <td>Halo</td>
+        </tr>
+        <tr>
+            <td>Reach</td>
+            <td>Reach</td>
+            <td>Reach</td>
+            <td>Reach</td>
+            <td>Reach</td>
+            <td>Reach</td>
         </tr>
         </table>
     </body>
